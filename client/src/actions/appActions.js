55d29fe651c9a3327766271d037a3cd6e536e75b
Derek Hoang <hoangda23@gmail.com>
2016-07-26 12:00:39 -0700

    nothing

--- client/src/actions/appActions.js
+++ client/src/actions/appActions.js
@@ -1,6 +1,6 @@
-//App Actions: methods available for the first children of AppRedux
+// App Actions: methods available for the first children of AppRedux
 
-//Import actions here
+// Import actions here
 import { fetchChallenges } from './fetchChallenges';
 import { fetchUsers } from './fetchUsers';
 import { loginUser } from './loginUser';
