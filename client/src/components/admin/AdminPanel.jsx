2d1ec18d16aac8b7425d1cf99336ac9bc3d1b088
Alex Stark <alexcstark@gmail.com>
2016-07-27 11:09:11 -0700

    fix funky string display

--- client/src/components/admin/AdminPanel.jsx
+++ client/src/components/admin/AdminPanel.jsx
@@ -6,6 +6,7 @@ import SelectField from 'material-ui/SelectField';
 import MenuItem from 'material-ui/MenuItem';
 import TextField from 'material-ui/TextField'
 import EditForm from './EditForm.jsx';
+import moment from 'moment';
 
 class AdminPanel extends React.Component {
   constructor(props) {
@@ -48,11 +49,10 @@ class AdminPanel extends React.Component {
         <CardText>
           <EditForm {...this.props} challenge={challenge}/>
           <div>{challenge.successes} out of {challenge.challengers} challengers have completed this challenge! </div>
-          <div>Current challengers: {challenge.currentChallengers}</div>
           <div>Completed: {challenge.successNames}</div>
           <br />
-          <div>Start: {challenge.createdAt} </div>
-          <div>End: {challenge.endTime} </div>
+          <div>Start: {moment(challenge.createdAt).format("MM/DD/YY")} </div>
+          <div>End: {moment(challenge.endTime).format("MM/DD/YY")} </div>
           <RaisedButton label="Delete this challenge" backgroundColor="#e22114"/>
           <PendingApprovalList players={this.props.playersOfUserChallenges[id]} handleClick={this.props.adminClick.bind(null, id)} entities={this.props.entities} />
         </CardText>
