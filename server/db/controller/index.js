7a160efb9f75e4c4f75364983b119c4e7c22d0b5
andrew-heath <aheath42@gmail.com>
2016-07-26 19:50:13 -0700

    Move portion of db call that gathers details into a helper function

--- server/db/controller/index.js
+++ server/db/controller/index.js
@@ -24,61 +24,10 @@ module.exports = {
           faceId = fid.passport.user.id;
         }
       }
-      let userInfo
-      let userObj;
-      console.log('facebook id', fid);
       // search for user from facebookId req.user is the session information stored in every req
       // model.User.find({ where: { facebookId: req.user.id } })
-      model.User.findOne({ where: { facebookId: faceId } })
-      .then((user) => {
-        userObj = user;
-        userInfo = user.dataValues;
-        // search for all challenges created by user
-        return model.Challenge.findAll({ where: { userId: user.dataValues.id } })
-      })
-      .then((challenges) => {
-        // saves all challenges created by user to challengesCreated
-        userInfo.challengesCreated = challenges.map(challenge => challenge.dataValues);
-        // search and save all challenges taken by user
-        // search Users_challenges table records with user's id
-        return model.Users_challenge.findAll({ where: { userId: userObj.dataValues.id } })
-      })
-      .then((userChallenges) => {
-        // for each entry in Uses'_challenge table search Challenes table for data
-        const arrayOfPromises = userChallenges.map(userChallenge =>
-           model.Challenge.find({ where: { id: userChallenge.dataValues.challengeId } })
-        );
-
-        return Promise.all(arrayOfPromises);
-      })
-      .then((arrayOfChallengesTaken) => {
-        // save all challenesTaken to results.challengesTaken
-        userInfo.challengesTaken = arrayOfChallengesTaken
-          .map(challengesTaken => challengesTaken.dataValues);
-        // search and save all challenges completed and approved
-        // search Users_challenges table to find all approved user challenges
-        return model.Users_challenge.findAll({
-          where: { userId: userObj.dataValues.id },
-          include: [{
-            model: model.Proof,
-            where: { creatorAccepted: true },
-          }],
-        });
-      })
-      .then((approvedChallenges) => {
-        const arrayOfApprovedChallenges = approvedChallenges
-          .map(approvedChallenge => model.Challenge
-              .find({ where: approvedChallenge.dataValues.challengeId }));
-        return Promise.all(arrayOfApprovedChallenges);
-      })
-      .then((resolvedChallenges) => {
-        userInfo.challengesCompleted = resolvedChallenges.map(challenge =>
-          challenge.dataValues);
-        // returns object will all users each of the challenges they have created,
-        // each of the challenges they have accepted, and each of the challenges they
-        // have completed
-        return res.json(userInfo);
-      });
+      console.log('before detailHelper');
+      detailHelper(req, res, faceId);
     },
   },
 
@@ -281,4 +230,63 @@ module.exports = {
       });
     },
   },
+};
+
+const detailHelper = (req, res, faceId) => {
+
+  console.log('in beginning of detailHelper');
+
+  let userInfo
+  let userObj;
+  
+  model.User.find({ where: { facebookId: faceId } })
+  .then((user) => {
+    userObj = user;
+    userInfo = user.dataValues;
+    // search for all challenges created by user
+    return model.Challenge.findAll({ where: { userId: user.dataValues.id } })
+  })
+  .then((challenges) => {
+    // saves all challenges created by user to challengesCreated
+    userInfo.challengesCreated = challenges.map(challenge => challenge.dataValues);
+    // search and save all challenges taken by user
+    // search Users_challenges table records with user's id
+    return model.Users_challenge.findAll({ where: { userId: userObj.dataValues.id } })
+  })
+  .then((userChallenges) => {
+    // for each entry in Uses'_challenge table search Challenes table for data
+    const arrayOfPromises = userChallenges.map(userChallenge =>
+       model.Challenge.find({ where: { id: userChallenge.dataValues.challengeId } })
+    );
+
+    return Promise.all(arrayOfPromises);
+  })
+  .then((arrayOfChallengesTaken) => {
+    // save all challenesTaken to results.challengesTaken
+    userInfo.challengesTaken = arrayOfChallengesTaken
+      .map(challengesTaken => challengesTaken.dataValues);
+    // search and save all challenges completed and approved
+    // search Users_challenges table to find all approved user challenges
+    return model.Users_challenge.findAll({
+      where: { userId: userObj.dataValues.id },
+      include: [{
+        model: model.Proof,
+        where: { creatorAccepted: true },
+      }],
+    });
+  })
+  .then((approvedChallenges) => {
+    const arrayOfApprovedChallenges = approvedChallenges
+      .map(approvedChallenge => model.Challenge
+          .find({ where: approvedChallenge.dataValues.challengeId }));
+    return Promise.all(arrayOfApprovedChallenges);
+  })
+  .then((resolvedChallenges) => {
+    userInfo.challengesCompleted = resolvedChallenges.map(challenge =>
+      challenge.dataValues);
+    // returns object will all users each of the challenges they have created,
+    // each of the challenges they have accepted, and each of the challenges they
+    // have completed
+    return res.json(userInfo);
+  });
 };
