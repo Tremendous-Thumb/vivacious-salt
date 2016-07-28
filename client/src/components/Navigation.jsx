3eff28450c25b6cc04a6b46cac623d2c3eb36045
andrew-heath <aheath42@gmail.com>
2016-07-27 20:46:42 -0700

    Reorder links in logical order when all show

--- client/src/components/Navigation.jsx
+++ client/src/components/Navigation.jsx
@@ -13,8 +13,8 @@ const Navigation = ({challenges, currentUser, entities, addPlayer, signUpChallen
   return (
     <nav className="yellow darken-2">
       <Link to="/"><span className="title">New Challenger</span></Link>
-      <span className="login"><a href="/auth/facebook"><img src="./images/facebook-login-button.png" onClick="" /></a></span>
-      <span className="logout"><a href="/logout">Log Out</a></span>
+      <span className="logout right linkStyle"><a href="/logout">Log Out</a></span>
+      <span className="login right linkStyle"><a href="/auth/facebook"><img src="./images/facebook-login-button.png" onClick="" /></a></span>
       {toProfile}
       <Link to="/challenges/create"><span className="right linkStyle">Create Challenge</span></Link>
     </nav>
